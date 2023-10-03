7fef0aab33cd845c35836f91fed66a2dd754cf1a
Wiktor Frątczak <wiktorft19@gmail.com>
2023-10-04 00:01:15 +0200

    added JaCoCo Test Reports to get test coverage

--- src/main/java/com/Spendify/Spendify/Invoice/InvoiceAddRequest.java
+++ src/main/java/com/Spendify/Spendify/Invoice/InvoiceAddRequest.java
@@ -1,9 +1,12 @@
 package com.Spendify.Spendify.Invoice;
 
 import lombok.Builder;
+import org.antlr.v4.runtime.misc.NotNull;
+
 @Builder
 public record InvoiceAddRequest(
                 Long userId,
+                @NotNull
                 Double price,
                 Double exchangeRate,
                 Long currencyId) { }
